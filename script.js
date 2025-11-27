267de599c547e1d088b5a593b0bf8637d5e943aa
SASROROHOT <cirillochristian08@gmail.com>
2025-11-27 19:03:10 +0100

    Refactor rendering and checkbox handling in script.js
    sbu

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 var index=0;
 function render(){
     while(localStorage.getItem(index) !== null){
-        document.getElementById('risposte').innerHTML += localStorage.getItem(index);
+        document.getElementById('risposte').innerHTML += '<div class="ss"> <input type="checkbox" class="mybox" onclick="cex('+index+')" '+ localStorage.getItem('n'+index)+' "><p class="boh">'+ localStorage.getItem(index) +'</p> </div>';
         index++;
     }
 }
+
 render();
 var testo;
+var help;
 document.getElementById('btn1').addEventListener('click', function(){
     event.preventDefault();
     if(document.getElementById('ins').value === '') return;
     testo=document.getElementById('ins').value;
-    
-    document.getElementById('risposte').innerHTML += '<div class="ss"> <input type="checkbox" class="mybox"><p class="boh">'+ testo +'</p> </div>';
-    localStorage.setItem(index++ , '<div class="ss"> <input type="checkbox"><p class="boh">'+ testo +'</p> </div>');
+    help='';
+    document.getElementById('risposte').innerHTML += '<div class="ss"> <input type="checkbox" class="mybox" onclick="cex('+index+')"'+help+'><p class="boh">'+ testo +'</p> </div>';
+    localStorage.setItem(index , testo);
+    localStorage.setItem('n'+index, help);
+    index++;
     document.getElementById('ins').value='';
     
 })
@@ -25,3 +29,15 @@ document.getElementById('clear').addEventListener('click', function(){
     });
    // window.location.reload();
 })
+
+function cex(e){
+    let stato = localStorage.getItem('n'+e);
+
+    if(stato === " checked "){
+        localStorage.setItem('n'+e, "");
+    } else {
+        localStorage.setItem('n'+e, " checked ");
+    }
+
+    window.location.reload();
+}
